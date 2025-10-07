2fc3623d70089b655e9c192ed45ed769ae4e4af7
Andrew Melo <andrew.melo@gmail.com>
2025-10-07 15:50:02 -0500

    Properly add default value

--- src/mlflow_mltf_gateway/resources/slurm-wrapper.sh
+++ src/mlflow_mltf_gateway/resources/slurm-wrapper.sh
@@ -5,7 +5,7 @@
 # support using "srun" to execute multiple copies of the same script
 #
 
-if [ "${SLURM_NTASKS:1}" -eq 1 ]; then
+if [ "${SLURM_NTASKS:-1}" -eq 1 ]; then
   {{command}}
 else
   srun --export=ALL {{command}}
